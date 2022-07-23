19ed7ac0ab90b2c7774d4433af8a3b9797a701cb
sakshamp11 <rimpi.saksham@gmail.com>
2022-07-24 01:38:51 +0530

    port changes

--- Jira/app.js
+++ Jira/app.js
@@ -5,8 +5,8 @@ let app = express();
 app.get(express.static('public'))
 // http://localhost:5000
 
-// let port = process.env.PORT || 5000;
-let port = 5000;
+let port = process.env.PORT || 5000;
+// let port = 5000;
 
 let server = app.listen(port, () => {
  console.log('app.Listern is Working');
